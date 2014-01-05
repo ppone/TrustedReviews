2ace7dcdd152fddb72524d02c8994fa45dd653ac
Parag Patel <parag.patel.one@gmail.com>
2014-01-05 16:50:19 -0500

    first commit Trusted Reviews

---
+++ social_integretions/yelp-integretion/yelp_test_connect.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+"fmt"
+)
+
+const (
+	CONSUMER_KEY  = "_eO9cEaS-bjF_1JYG9jRig"
+	TOKEN = "cHCHosp_uUfISZwdlA3wzmipFL9xA8KL"
+	CONSUMER_SECRET = "pNbOtQaCCuTl1w_JaCgkjRjtmL8"
+	TOKEN_SECRET = "SMI4T3k6FoIuDeDLqnISovQ4vRw"
+)
+
+func main() {
+	fmt.Println("Testing Yelp API Connection")
+	fmt.Printf("CONSUMER_KEY = %s, TOKEN = %s, CONSUMER_SECRECT = %s, TOKEN_SECRET = %s\n",CONSUMER_KEY,TOKEN,CONSUMER_SECRET, TOKEN_SECRET)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
