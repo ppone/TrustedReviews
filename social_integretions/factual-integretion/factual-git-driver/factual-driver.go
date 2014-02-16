fbaed5906ef2338d4aad8581ac793795f7ed930d
Parag Patel <parag.patel.one@gmail.com>
2014-02-16 17:06:38 -0500

    Returns successfull results.  Need to store data using json, this is the next step.

--- social_integretions/factual-integretion/factual-git-driver/factual-driver.go
+++ social_integretions/factual-integretion/factual-git-driver/factual-driver.go
@@ -3,16 +3,17 @@ package main
 import "oauth1"
 import "fmt"
 import "net/http"
+import "io/ioutil"
 
 func main() {
 	token := oauth1.NewAccessToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO", "", "")
-	fmt.Println(token)
+
 	request, err := http.NewRequest("GET", "http://api.v3.factual.com/t/places/03c26917-5d66-4de9-96bc-b13066173c65", nil)
-	//request, err := http.Get("http://api.v3.factual.com/t/places/03c26917-5d66-4de9-96bc-b13066173c65")
+
 	client := &http.Client{}
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR: ", err)
 	}
 
 	token.SignRequestHeader(request)
@@ -20,10 +21,17 @@ func main() {
 	result, err := client.Do(request)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR: ", err)
 	}
 
-	fmt.Printf("%s", result)
+	content, err := ioutil.ReadAll(result.Body)
 
-	//defer request.Body.Close()
+	defer result.Body.Close()
+
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	fmt.Printf("%s", content)
+
 }
