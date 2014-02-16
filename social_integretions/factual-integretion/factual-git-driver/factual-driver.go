b4d3eb763ddb99020aa6f90befde9fecaf99cdd7
Parag Patel <parag.patel.one@gmail.com>
2014-02-16 15:49:56 -0500

    created barebone version that compiles

--- social_integretions/factual-integretion/factual-git-driver/factual-driver.go
+++ social_integretions/factual-integretion/factual-git-driver/factual-driver.go
@@ -1,10 +1,9 @@
-package main 
+package main
 
 import "oauth1"
 import "fmt"
 
-package func main() {
-	
-
-
+func main() {
+	token := oauth1.NewAccessToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO", "", "")
+	fmt.Println(token)
 }
