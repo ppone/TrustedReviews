fd4b3a8dbbf811014f419773c0239de798b63b09
Parag Patel <parag.patel.one@gmail.com>
2014-02-16 16:06:15 -0500

    added more code

--- social_integretions/factual-integretion/factual-git-driver/factual-driver.go
+++ social_integretions/factual-integretion/factual-git-driver/factual-driver.go
@@ -2,8 +2,17 @@ package main
 
 import "oauth1"
 import "fmt"
+import "net/http"
 
 func main() {
 	token := oauth1.NewAccessToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO", "", "")
 	fmt.Println(token)
+	request, err := http.NewRequest("GET", "http://host.net/resource?a=b&c=d", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	token.SignRequestQuery(request)
+	fmt.Println(request)
 }
