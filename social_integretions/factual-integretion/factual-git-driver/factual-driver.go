629ba00ca7333c0a26cd844936a8f71adeb6fd86
Parag Patel <parag.patel.one@gmail.com>
2014-02-16 16:45:50 -0500

    Able to make a successfull request to factual (http 200).
    Still have to make sure I get the correct results.

--- social_integretions/factual-integretion/factual-git-driver/factual-driver.go
+++ social_integretions/factual-integretion/factual-git-driver/factual-driver.go
@@ -7,12 +7,23 @@ import "net/http"
 func main() {
 	token := oauth1.NewAccessToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO", "", "")
 	fmt.Println(token)
-	request, err := http.NewRequest("GET", "http://host.net/resource?a=b&c=d", nil)
+	request, err := http.NewRequest("GET", "http://api.v3.factual.com/t/places/03c26917-5d66-4de9-96bc-b13066173c65", nil)
+	//request, err := http.Get("http://api.v3.factual.com/t/places/03c26917-5d66-4de9-96bc-b13066173c65")
+	client := &http.Client{}
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	token.SignRequestQuery(request)
-	fmt.Println(request)
+	token.SignRequestHeader(request)
+
+	result, err := client.Do(request)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%s", result)
+
+	//defer request.Body.Close()
 }
