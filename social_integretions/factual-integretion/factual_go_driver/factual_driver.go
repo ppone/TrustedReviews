e63e672470258beb675cd7c7d35a1071d34aac63
Parag Patel <parag.patel.one@gmail.com>
2014-08-29 09:30:50 -0500

    Added generic Get(url string) method on a *oauth1.AccessToken type. Removed factualQuery and added datastructures tokenOauth and factualRead.  Changed previous GetXXX Method on factualQuery to be on factualRead and incorporated the calling of the generic Get() Method.

--- social_integretions/factual-integretion/factual_go_driver/factual_driver.go
+++ social_integretions/factual-integretion/factual_go_driver/factual_driver.go
@@ -1,12 +1,14 @@
-package main
+package factual
 
 import "oauth1"
-import "fmt"
+
+//import "fmt"
 import "net/http"
 import "net/url"
 import "io/ioutil"
 import "./read"
 import "errors"
+import "fmt"
 
 //import "strings"
 
@@ -23,48 +25,67 @@ func EncodeURLParameters(urlstring string) string {
 	return encodedURLValue.String()
 }
 
-type factualRequest struct {
-	token *oauth1.AccessToken
+type tokenOauth struct {
+	*oauth1.AccessToken
+}
+
+type factualRead struct {
+	tokenOauth
 	read.Read
 }
 
-func NewRequest(consumerKey, consumerSecret string) *factualRequest {
-	f := new(factualRequest)
-	f.token = oauth1.NewAccessToken(consumerKey, consumerSecret, "", "")
-	return f
+func NewToken(consumerKey, consumerSecret string) tokenOauth {
+	t := tokenOauth{oauth1.NewAccessToken(consumerKey, consumerSecret, "", "")}
+	return t
 }
 
-func (F *factualRequest) NewRead(tableName string) (*factualRequest, error) {
+func NewRead(t tokenOauth, tableName string) (*factualRead, error) {
+	fR := new(factualRead)
 	f, err := read.NewReader(tableName)
 	if err != nil {
 		return nil, err
 	}
-	F.Read = f
-	return F, nil
+	fR.tokenOauth = t
+	fR.Read = f
+
+	return fR, nil
 
 }
 
-func (F *factualRequest) GetReadURL() string {
+func (F *factualRead) GetReadURL() string {
 	return FACTUAL_BASE_URL + F.String()
 }
 
-func (F *factualRequest) GetRead() (string, error) {
+func (F *factualRead) Get() (string, error) {
 	if F.Read == nil {
 		return "", errors.New("Factual Request has no read data")
 	}
-	encodedURL := EncodeURLParameters(F.GetReadURL())
+
+	s, err := F.tokenOauth.Get(F.GetReadURL())
+
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+
+}
+
+func (T tokenOauth) Get(url string) (string, error) {
+	encodedURL := EncodeURLParameters(url)
 	request, err := http.NewRequest("GET", encodedURL, nil)
 	if err != nil {
 		return "", err
 	}
 	client := &http.Client{}
-	F.token.SignRequestHeader(request)
+	T.SignRequestHeader(request)
 	result, err := client.Do(request)
 	if err != nil {
 		return "", err
 	}
 	defer result.Body.Close()
 
+	fmt.Println("Throttle Allocation => ", result.Header.Get("X-Factual-Throttle-Allocation"))
 	content, err := ioutil.ReadAll(result.Body)
 
 	if err != nil {
@@ -72,91 +93,5 @@ func (F *factualRequest) GetRead() (string, error) {
 	}
 
 	return string(content), nil
-
-}
-
-func main() {
-
-	fR := NewRequest("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO")
-
-	_, err := fR.NewRead("restaurants-us")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fR.AddQuery("pizza", "sushi")
-	fR.AddLimit(5)
-	fR.AddSelect("factual_id")
-
-	fmt.Println("frData", fR.GetReadURL())
-
-	s, err := fR.GetRead()
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(s)
-
-	//token := oauth1.NewAccessToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO", "", "")
-	/*
-		r, err := read.NewRead("restaurants-us")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		r, err = r.AddQuery("Coffee", "\"Los Angeles\"")
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		r = r.AddLimit(5)
-		//r, err = r.AddSelect("cuisine")
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}*/
-
-	/*fmt.Println("TESTING RAW QUERY STRING", FACTUAL_BASE_URL+r.String())
-	urlstring := EncodeURLParameters(FACTUAL_BASE_URL + r.String())
-	durlstring := EncodeURLParameters("http://api.v3.factual.com/t/restaurants-us?q=Coffee,\"Los Angeles\"&limit=5&select=factual_id")
-	fmt.Println("TESTING QUERY STRING", durlstring, urlstring)
-	request, err := http.NewRequest("GET", durlstring, nil)
-	client := &http.Client{}
-
-	if err != nil {
-		fmt.Println("ERROR: ", err)
-	}
-
-	token.SignRequestHeader(request)
-
-	result, err := client.Do(request)
-
-	if err != nil {
-		fmt.Println("ERROR: ", err)
-	}
-
-	//_, err := ioutil.ReadAll(result.Body)
-	content, err := ioutil.ReadAll(result.Body)
-
-	defer result.Body.Close()
-
-	if err != nil {
-		fmt.Println("ERROR: ", err)
-	}
-
-	//fmt.Println(request)
-	fmt.Println("result => ", result)
-
-	fmt.Println("request => ", request, "REQUEST_URL => ", request.URL)
-	fmt.Printf("%s", content)
-	*/
 
 }
