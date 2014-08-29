f1a3bec6a4c445c60bb0c10c2569b7d012eb22df
Parag Patel <parag.patel.one@gmail.com>
2014-08-29 09:33:50 -0500

    Modifed tests to now call AddQuery, AddLimit, etc on factualRead.  Added another test to, test out the generic Get method

--- social_integretions/factual-integretion/factual_go_driver/factual_driver_test.go
+++ social_integretions/factual-integretion/factual_go_driver/factual_driver_test.go
@@ -1,1 +1,50 @@
-package main
+package factual
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestToken(t *testing.T) {
+	fR := NewToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO")
+
+	nR, err := NewRead(fR, "restaurants-us")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	nR.AddQuery("pizza")
+	nR.AddLimit(5)
+	nR.AddSelect("factual_id")
+
+	fmt.Println("frData", nR.GetReadURL())
+
+	s, err := nR.Get()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if strings.Contains(s, "error") {
+		t.Error("response returned an error")
+	}
+
+	fmt.Println(s)
+
+}
+
+func TestSchema(t *testing.T) {
+	fR := NewToken("Pbu7jRdBErgLW07g9c25JtGcwwt1KmpoxRTfFL3x", "vC4AgocPBhxe0GFkTsetoiuEAJEgqz6MCbAnXEoO")
+	_, err := fR.Get("http://api.v3.factual.com/t/restaurants-us/schema")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	//fmt.Println(s)
+
+}
